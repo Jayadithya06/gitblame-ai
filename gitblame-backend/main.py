48cf4ecb3cd9882253ce6a70456fa80358153a92
Jayadithya Akunuri <Akunuri>
2026-06-26 15:24:15 +0530

    Lock down CORS for production

--- gitblame-backend/main.py
+++ gitblame-backend/main.py
@@ -11,7 +11,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+    "http://localhost:5173",
+    "https://gitblame-ai-frontend.onrender.com"
+],
 )
 
 app.include_router(auth.router)
